bb470be31bd9d91f9e7d9bf8c831105ea09bb928
Takeshi Takatsudo <takazudo@gmail.com>
2013-09-03 21:42:52 +0900

    bumped version number to v1.6.3

--- jquery.touchdragh.js
+++ jquery.touchdragh.js
@@ -1,6 +1,6 @@
 /*! jQuery.touchdragh (https://github.com/Takazudo/jQuery.touchdragh)
  * lastupdate: 2013-09-03
- * version: 1.6.2
+ * version: 1.6.3
  * author: 'Takazudo' Takeshi Takatsudo <takazudo@gmail.com>
  * License: MIT */
 (function() {
